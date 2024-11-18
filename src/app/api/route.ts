59a18df59428f83a8c7a4e41728ff949fb37edf5
animesh65432 <kiranduttta234@gmail.com>
2024-11-18 13:27:16 +0530

    try to fix

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server"
 import axios from "axios"
 export const GET = async () => {
     try {
-        const reponse = await axios.get(`${process.env.BACKEND_URL}`)
+        const reponse = await axios.get(`https://flipkart-email-mock.vercel.app`)
         console.log(reponse, "Response From Backend")
         console.log(reponse?.data)
         return NextResponse.json({
